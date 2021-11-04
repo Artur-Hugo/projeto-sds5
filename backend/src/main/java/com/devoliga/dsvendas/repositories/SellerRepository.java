af4ae43ee9fb2a22b625e7f2a741d1ee4fcc9032
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-11-04 01:32:51 -0300

    Layers

---
+++ backend/src/main/java/com/devoliga/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devoliga.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devoliga.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/devoliga/dsvendas/service/SellerService.java
@@ -0,0 +1,25 @@
+package com.devoliga.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devoliga.dsvendas.dto.SellerDTO;
+import com.devoliga.dsvendas.entities.Seller;
+import com.devoliga.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
